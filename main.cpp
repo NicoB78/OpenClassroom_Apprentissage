fb84a83454cbc96ebf3871f3041617cd5ea20633
Nicolas Beque <nicolas.beque@epfedu.fr>
2022-10-29 15:54:41 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream> // Inclut la bibliotheque iostream (affichage de texte)
+
+using namespace std; //Indique quel espace de noms on va utiliser
+
+
+/*
+Fonction principale main
+Tous les programmes commencent par la fonction main
+*/
+int main()
+{
+    cout << "Hello world!" << endl << "Changement de ligne effectue" << endl;
+    cout <<"Affichage backslash et guillemets  \" \\ " << endl;
+    //Commentaire
+    /* Commentaire */
+
+    return 0;
+}
